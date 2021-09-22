05c8390e18da601b21e8ac43b390c5a6754d737d
Hackit321 <kirwamarkk@gmail.com>
2021-09-22 20:40:29 +0300

    created a crecential class and credentail list array

--- passlock.py
+++ passlock.py
@@ -21,7 +21,37 @@ class User:
 
 
 
-    
+    def  save_user(self):
+
+        '''
+        method that saves a new instances into the user list
+        '''
+        User.user_list.append(self)
+
+
+    @classmethod
+    def display_user():
+        return cls.user_list
+
+
+
+    def delete_user(self):
+
+        '''
+        method that delete user details
+        '''
+        self.user_list.remove(self)
+
+
+class Credentials:
+
+    '''
+    credentials class that create new objects for credentials
+    '''
+
+    Credentials_list =[]
+
+   
 
 
 
