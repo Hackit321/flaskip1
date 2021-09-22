6bcc0c6f1eb28c6655feaa38973548af8b494498
Hackit321 <kirwamarkk@gmail.com>
2021-09-22 20:17:12 +0300

    initializdd the username and password

--- passlock.py
+++ passlock.py
@@ -2,13 +2,33 @@ import random
 import string
 
 
+class User:
+    '''
+    class that generates new instance of a user
+    '''
 
-class User():
+    user_list = []
+
+    def __init__(self,username,password):
+
+        '''
+        method that defines the properties of user
+        '''
+
+        self.username = username
+        self.password = password
 
 
 
 
+    
 
-class Credentials():
+
+
+    
+
+
+    
+
 
 
