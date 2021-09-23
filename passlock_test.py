7156ceab5dc6341c7793b1ad736f6e7acb4e130a
Hackit321 <kirwamarkk@gmail.com>
2021-09-23 12:39:26 +0300

    created a function that check if user list has been saved and checks the number of objects saved

--- passlock_test.py
+++ passlock_test.py
@@ -21,6 +21,14 @@ class TestClass(unittest.TestCase):
         self.assertEquals(self.new_user.password,'mark002.')
 
 
+    def test_save_user(self):
+        '''
+        test case to test if useer object is saved in the user list    
+        '''
+        self.new_user.save_user()
+        self.assertEquals(len(User.user_list),1)
+
+
 
 
 
