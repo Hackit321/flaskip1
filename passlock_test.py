f5a8b3d3ad13532d83f168b3d04ac1dc010ff5a6
Hackit321 <kirwamarkk@gmail.com>
2021-09-23 12:28:21 +0300

    created user testcase setup

--- passlock_test.py
+++ passlock_test.py
@@ -2,6 +2,33 @@ import unittest
 from passlock import User,Credentials
 
 class TestClass(unittest.TestCase):
+    '''
+    A Test class that defines test case for the user  behaviour
+    '''
+    def setUp(self) :
+        '''
+        set up mehtod that runs each testcase
+        '''
+        self.new_user = User('mark','mark002.')
+
+        return super().setUp()
+    
+    def test_init(self):
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class TestCredentials(unittest.TestCase):
+    '''
+    A Test class that defines test case for the credentials
+    '''
