7dc89165a8f53db43ff4042b3272254834e11877
Hackit321 <kirwamarkk@gmail.com>
2021-09-23 16:43:31 +0300

    created functions that create user,save user then display also check if user can login

--- interface.py
+++ interface.py
@@ -7,7 +7,31 @@ def logo():
 
 
 
+def create_new_user(username,password):
+    '''
+    function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user
 
+def save_user(username,password):
+    '''
+    function to save a new user
+    '''
+    User.save_user(username,password)
+
+def display_user():
+    '''
+    function to display existing user
+    '''
+    return User.display_user()
+
+def login_user(username,password):
+    '''
+    function that checks whether a user exist and then login the user on
+    '''
+    check_user = Credentials.verify_user(username,password)
+    return check_user
 
 
 
